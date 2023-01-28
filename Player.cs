d916bb2e7b59047d38db174e6b9e65ec8326b36e
Sakari Ekqvist <sakari.ekqvist@magicad.com>
2023-01-28 20:40:45 +0200

    Force from input

--- Player.cs
+++ Player.cs
@@ -1,12 +1,18 @@
 using Godot;
-using System;
 
 public partial class Player :  RigidBody3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ApplyImpulse(new Vector3(100, 0, 0));
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event.IsAction("Fire"))
+		{
+			ApplyImpulse(new Vector3(100, 0, 0));
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
