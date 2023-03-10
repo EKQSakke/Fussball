29fbc5af3af963ebf8ceb5d0d3fc38ecaa201451
Sakari Ekqvist <ekq.sakke@gmail.com>
2023-03-11 00:00:35 +0200

    Formatting

--- Networker.cs
+++ Networker.cs
@@ -5,36 +5,36 @@ using Nidot;
 public partial class Networker : Node
 {
 
-	[Export] PackedScene player;
-	Node networked;
+    [Export] PackedScene player;
+    Node networked;
 
-	Dictionary<int, long> playerTeamIds = new();
-	PlayerPositioner playerPositioner = new();
+    Dictionary<int, long> playerTeamIds = new();
+    PlayerPositioner playerPositioner = new();
 
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		var lobby = this.GetNodeFromAll<Lobby>();
-		networked = this.GetNodeFromAll<MultiplayerSpawner>();
-		playerPositioner.AddSpawnPoints(GetNode("SpawnPoints").GetNodesOfType<SpawnPoint>());
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        var lobby = this.GetNodeFromAll<Lobby>();
+        networked = this.GetNodeFromAll<MultiplayerSpawner>();
+        playerPositioner.AddSpawnPoints(GetNode("SpawnPoints").GetNodesOfType<SpawnPoint>());
+    }
 
-	
-	public void SpawnTeam(long id)
+
+    public void SpawnTeam(long id)
     {
         GD.Print($"{id} connected");
 
-		var teamId = playerTeamIds.Count;
-		playerTeamIds.Add(playerTeamIds.Count, id);
+        var teamId = playerTeamIds.Count;
+        playerTeamIds.Add(playerTeamIds.Count, id);
 
-		for (int i = 0; i < Globals.PlayerPerTeamCount; i++)
-		{
-			var newPlayerNode = player.Instantiate();
-			var playerNode = newPlayerNode as Player ?? throw new System.Exception("playerNode must be a Player");
-			playerNode.GlobalPosition = playerPositioner.GetNextSpawnPointForTeam(teamId);
-			playerNode.PlayerId = id;
-	        networked.AddChild(newPlayerNode, true);
-		}
+        for (int i = 0; i < Globals.PlayerPerTeamCount; i++)
+        {
+            var newPlayerNode = player.Instantiate();
+            var playerNode = newPlayerNode as Player ?? throw new System.Exception("playerNode must be a Player");
+            playerNode.GlobalPosition = playerPositioner.GetNextSpawnPointForTeam(teamId);
+            playerNode.PlayerId = id;
+            networked.AddChild(newPlayerNode, true);
+        }
     }
 }
